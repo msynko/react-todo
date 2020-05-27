bb95b0fca9b382392645193ec742d1f97bbfbfb1
m_sinko9730 <maryna@thinkingbox.com>
2020-05-27 12:30:19 -0400

    Updates

---
+++ precache-manifest.b7006dfa75377b975047f09ee996c531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e679aa1cb2d8643a448f045bc34dd8",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "7d94a725cc6de7b176b1",
+    "url": "/react-todo/static/css/main.05e48b9e.chunk.css"
+  },
+  {
+    "revision": "fdb9c1327478e373a1dc",
+    "url": "/react-todo/static/js/2.27df9a86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.27df9a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d94a725cc6de7b176b1",
+    "url": "/react-todo/static/js/main.c50f7413.chunk.js"
+  },
+  {
+    "revision": "128de4d092902628bfba",
+    "url": "/react-todo/static/js/runtime-main.1d0efd3c.js"
+  }
+]);
